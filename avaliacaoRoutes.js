4d9560bb21ffd7acdedf2ed4a4a3d8f69605ae96
joaoeduardo-max <12242007jo@gmail.com>
2026-04-17 23:30:38 -0300

    Delete avaliacaoRoutes.js

--- avaliacaoRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import AvaliacaoController from '../controllers/AvaliacaoController.js';
-
-const router = Router();
-
-router.post('/', AvaliacaoController.store);
-
-export default router;
